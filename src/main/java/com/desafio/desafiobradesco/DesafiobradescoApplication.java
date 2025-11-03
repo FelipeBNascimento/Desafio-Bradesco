cd807edc22b767606e06755a3984f0dc717d61d6
Bazan <felipe_bazan3006@hotmail.com>
2025-11-03 17:35:01 -0300

    Commit inicial

---
+++ src/main/java/com/desafio/desafiobradesco/DesafiobradescoApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiobradesco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiobradescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiobradescoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/desafiobradesco/DesafiobradescoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.desafiobradesco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiobradescoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
